ed7ff3af28b9afba63f5d543dcb004d9f889c0e8
Vibhor Mechu <vibhormechu@vibhors-mbp.mynetworksettings.com>
2023-11-22 23:08:00 -0500

    Added Project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import MapView from '../nyc-wifi-hotspots/src/components/MapView';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>NYC Wi-Fi Hotspots Explorer</h1>
+            </header>
+            <main>
+                <MapView />
+            </main>
+            <footer>
+                <p>© 2023 NYC Wi-Fi Hotspots Project</p>
+            </footer>
+        </div>
+    );
+}
+
+export default App;
+
