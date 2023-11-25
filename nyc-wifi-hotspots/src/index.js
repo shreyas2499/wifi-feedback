b0402ab47b412856a6b2536c17658e167b61e18a
Shreyas K N <shreyas2499@gmail.com>
2023-11-25 16:53:58 -0500

    Removed extra import

--- nyc-wifi-hotspots/src/index.js
+++ nyc-wifi-hotspots/src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
